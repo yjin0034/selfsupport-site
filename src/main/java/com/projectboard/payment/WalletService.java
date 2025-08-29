7153b1a2bf45c9d5b0d18c48f801321e52b729eb
yjin0034 <yjin0034@naver.com>
2025-08-29 16:26:40 +0900

    #9 - style(payment): WalletService의 불필요한 import 제거
    - 사용하지 않는 `java.time.LocalDateTime` import 삭제

--- src/main/java/com/projectboard/payment/WalletService.java
+++ src/main/java/com/projectboard/payment/WalletService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
